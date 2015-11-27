a4eec70aed3f0e0c18ba67fa0c357eaf6f68e350
Hilton Perantunes <hperantunes@gmail.com>
2015-11-27 14:26:38 -0500

    minor fix

--- CNRailway/CNRailway.MarshallingYard/Movement.cs
+++ CNRailway/CNRailway.MarshallingYard/Movement.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CNRailway.Util;
 
 namespace CNRailway.MarshallingYard
 {
-    public class Movement
+    public class Movement : IMovement
     {
         public INamed OriginLine { get; private set; }
 
